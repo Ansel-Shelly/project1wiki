23f5e1d67a6899ffa8d4016b06cc8b65357a102e
dundun305 <dundun305@gmail.com>
2020-12-22 15:56:05 -0500

    fixed all encyclopedia links

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("css", views.css, name="CSS"),
+    path("django", views.django, name="Django"),
+    path("git", views.git, name="Git"),
+    path("html", views.html, name="HTML"),
+    path("python", views.python, name="Python")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,4 +7,23 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def css(request):
+    return render(request,"encyclopedia/css.html",{
+        "content": util.get_entry("CSS")
+    })
+def django(request):
+    return render(request,"encyclopedia/django.html",{
+        "content": util.get_entry("Django")
+    })
+def git(request):
+    return render(request,"encyclopedia/git.html",{
+        "content": util.get_entry("Git")
+    })
+def html(request):
+    return render(request,"encyclopedia/html.html",{
+        "content": util.get_entry("HTML")
+    })
+def python(request):
+    return render(request,"encyclopedia/python.html",{
+        "content": util.get_entry("Python")
+    })
