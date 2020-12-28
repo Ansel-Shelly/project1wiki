2b68f51ca7c2f7c1f95944dc9f30b99170ee26a1
dundun305 <dundun305@gmail.com>
2020-12-28 18:44:25 -0500

    re-engineered the dynamic nature of how the application functions

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("css", views.css, name="CSS"),
-    path("django", views.django, name="Django"),
-    path("git", views.git, name="Git"),
-    path("html", views.html, name="HTML"),
-    path("python", views.python, name="Python")
+    path("wiki/<str:site>", views.site, name="site")
+    #path("django", views.django, name="Django"),
+    #path("git", views.git, name="Git"),
+    #path("html", views.html, name="HTML"),
+    #path("python", views.python, name="Python")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,28 +2,19 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-def css(request):
-    return render(request,"encyclopedia/css.html",{
-        "content": util.get_entry("CSS")
-    })
-def django(request):
-    return render(request,"encyclopedia/django.html",{
-        "content": util.get_entry("Django")
-    })
-def git(request):
-    return render(request,"encyclopedia/git.html",{
-        "content": util.get_entry("Git")
-    })
-def html(request):
-    return render(request,"encyclopedia/html.html",{
-        "content": util.get_entry("HTML")
-    })
-def python(request):
-    return render(request,"encyclopedia/python.html",{
-        "content": util.get_entry("Python")
+
+def site(request, site):
+    if util.get_entry(site):
+        content = markdown2.markdown(util.get_entry(site))
+    else:
+        content = None
+    return render(request, "encyclopedia/site.html", {
+        "site": site,  
+        "content": content
     })
